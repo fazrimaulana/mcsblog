7c06088ee464bcc4b35e8942e5d91da911a69107
fazri <fm.fazri.m@gmail.com>
2017-04-21 15:46:51 +0700

    update page theme

--- modules/Appearance/Views/themes/index.blade.php
+++ modules/Appearance/Views/themes/index.blade.php
@@ -1,1 +1,43 @@
-Themes
+@extends('layouts.backend')
+
+@section('content')
+        <!-- HEADER-POST -->
+        <div class="header-title">
+            <h3>
+                <i class="fa fa-image" aria-hidden="true"></i> Themes <!-- <a href="{{ url('/dashboard/media/add') }}" class="btn btn-sm btn-4 btn-info" >Add New</a> -->
+            </h3>
+        </div><!--END OF HEADER-POST-->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <!--.CONTENT-GALLERY-->
+            <div class="content-gallery">
+
+                <div class="gallery">
+                    <div class="header-gallery">
+                        <!-- <div class="title pull-left">
+                            <h3>Photo</h3>
+                        </div> --><!--END OF TITLE-->
+                    </div><!--END OF HEADER-GALLERY-->
+                    <div class="body-gallery">
+                        
+                        
+                        
+                        <div class="clearfix"></div>
+                    </div><!--END OF BODY-GALLERY-->
+                </div><!--END OF GALLERY-->
+            
+            </div><!--END OF .CONTENT-GALLERY-->
+
+        	
+        </div>
+        <!-- /#page-content-wrapper -->
+
+@endsection
+
+@section('javascript')
+	
+	
+
+@stop
